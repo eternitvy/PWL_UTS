847e3a6b316b663150dec1fc94933caeba7adc4d
eternitvy <crrissant@gmail.com>
2026-04-21 03:49:07 +0700

    CRUD Resource Barang

--- app/Filament/Resources/Barangs/Schemas/BarangForm.php
+++ app/Filament/Resources/Barangs/Schemas/BarangForm.php
@@ -3,6 +3,13 @@
 namespace App\Filament\Resources\Barangs\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Schemas\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Actions\Action;
 
 class BarangForm
 {
@@ -10,7 +17,56 @@ class BarangForm
     {
         return $schema
             ->components([
-                //
+                Wizard::make([
+                    Step::make('Info Barang')
+                        ->icon('heroicon-o-information-circle')
+                        ->description('Isi Informasi Barang')
+                        ->schema([
+                            Group::make([
+                                Group::make([
+                                    Select::make('kategori_id')
+                                        ->relationship('kategori', 'kategori_nama')
+                                        ->label('Nama Kategori')
+                                        ->required()
+                                        ->searchable()
+                                        ->preload(),
+
+                                    TextInput::make('barang_nama')
+                                        ->label('Nama Barang')
+                                        ->required(),
+
+                                    TextInput::make('barang_kode')
+                                        ->label('Kode Barang')
+                                        ->required(),
+                                ])->columnSpan(1),
+                                Group::make([
+                                    FileUpload::make('image')
+                                        ->label('Foto Barang')
+                                        ->image()
+                                        ->directory('barang-images'),
+                                ])->columnSpan(1),
+                            ])->columns(2),
+                        ]),
+
+                    Step::make('Harga')
+                        ->icon('heroicon-o-currency-dollar')
+                        ->description('Isi Harga Produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('harga_beli')
+                                    ->label('Harga Beli')
+                                    ->numeric()
+                                    ->prefix('Rp')
+                                    ->required(),
+                                TextInput::make('harga_jual')
+                                    ->label('Harga Jual')
+                                    ->numeric()
+                                    ->prefix('Rp')
+                                    ->required(),
+                            ])->columns(2),
+                        ]),
+                ])
+                ->columnSpanFull()
             ]);
     }
 }
--- app/Filament/Resources/Barangs/Tables/BarangsTable.php
+++ app/Filament/Resources/Barangs/Tables/BarangsTable.php
@@ -6,6 +6,8 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
 
 class BarangsTable
 {
@@ -13,7 +15,21 @@ class BarangsTable
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('image') 
+                    ->label('Foto')
+                    ->disk('public') 
+                    ->circular(),
+                TextColumn::make('kategori.kategori_nama')
+                    ->label('Kategori'), 
+                TextColumn::make('barang_kode')
+                    ->label('Kode'),
+                TextColumn::make('barang_nama')
+                    ->label('Nama'),
+                TextColumn::make('harga_beli')
+                    ->label('Harga Beli'),
+                TextColumn::make('harga_jual')
+                    ->label('Harga Jual'),
+                
             ])
             ->filters([
                 //
